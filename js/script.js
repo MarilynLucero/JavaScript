c323011a7baa7aededcd82ab4b4e957ba5c16df8
Marilyn Lucero <lu_tina_lu@hotmail.com>
2022-07-24 22:34:42 -0300

    ärrays

--- js/script.js
+++ js/script.js
@@ -7,80 +7,155 @@ const iva = (x) => (x * 0.21);
 const suma = (a,b) => a + b;
 const descuento = (x, y) => x * y;
 const interes = descuento;
+const carritoLibro= [];
+const libroStock = [{ nombre:'Cementerio De Animales', autor:'Stephen King', editorial:'Alfaguara', costo:'$5100'}, {nombre:'Battle Royale', autor:'Koushun Takami', editorial:'Booket', costo:'$2500'},{nombre:'La Vida Invisible De Addie Larue', autor:'Victoria Schwab', editorial:'Urano', costo:'$3500'},{nombre:'Una Breve Historia De Casi Todo', autor:'Bryson Bill', editorial:'Corre la voz', costo:'$2400'},{nombre:'Asesino De Brujas, La Bruja Blanca', autor:'Shelby Mahurin', editorial:'PUCK', costo:'$2900'}];
 
+function menu(){
+    let menuLibro= prompt('Elegí la opción deseada: \n1-Consulta de stock. \n2-Cotizacion del producto. \n3-Mi carrito.')
 
-let precioLibro = parseInt(prompt('Ingresá el valor de tu libro'));
-let formaDePago = prompt(`Ingresá tu metodo de pago  
-1-Efectivo
-2-Tarjeta de crédito`)
+    switch(menuLibro){
+        case '1':
+            Consulta();
+            break;
+        
+        case '2':
+            Cotizacion();
+            break;    
 
-const precioConIva = suma(precioLibro,iva(precioLibro));
-let precioTotal = 0;
-
-if(formaDePago=='1'){
-    const descuentoAplicado = descuento(precioConIva, 0.05);
-    precioTotal = precioConIva-descuentoAplicado;
-    alert(precioTotal); 
-
-
-} else if (formaDePago==2){
-    let debitoCredito = prompt('Va a ser con debito o credito? \n1-Débito \n2-Crédito')
-        switch (debitoCredito) {
-            case '1':
-                //Pago con tarjeta debito
-                let santanderTarjeta = prompt('Vas a pagar con santanderio rio? \n1-Si \n2-No')
-                switch(santanderTarjeta){
-                    case '1':
-                        //Pago con santander rio
-                        const descuentoSantander = descuento(precioConIva, 0.10);
-                        precioTotal = precioConIva-descuentoSantander;
-                        alert(precioTotal);
-                        break;
-                     case '2':
-                        //Pago con otra tarjeta
-                        precioTotal = precioConIva;
-                        alert(precioTotal);
-                        break;
-                    default:
-                    alert('No existe esa opción ingresada, intenta nuevamente');
-                    break;
-                }
-                break;
-            case '2':
-                //Pago con tarjeta de credito
-                let cantidadCuotas = prompt('En cuantas cuotas vas a abonar? \n1-1 cuota \n2-3 cuotas \n3-6 cuotas')
-                let precioconInteres = 0;
-                let precioCuota =0;
-                switch(cantidadCuotas){
-                    case '1':
-                        //1 cuota
-                        precioTotal = precioConIva;
-                        alert(precioTotal);
-                        break;
-                    case '2':
-                        //3 cuotas
-                        precioconInteres = interes(precioConIva, 0.08);
-                        precioTotal = precioConIva+precioconInteres;
-                        precioCuota= precioTotal/3
-                        alert('El valor total de tu compra es ' + precioTotal + ' y el valor de tus cuotas es: ' + precioCuota);
-                        break;
-                    case '3':
-                        //6 cuotas
-                        precioconInteres = interes(precioConIva, 0.12);
-                        precioTotal = precioConIva+precioconInteres;
-                        precioCuota= precioTotal/6
-                        alert('El valor total de tu compra es ' + precioTotal + ' y el valor de tus cuotas es: ' + precioCuota);
-                        break;
-                     default:
-                     alert('No existe esa opción ingresada, intenta nuevamente');
-                    break;
-                }
-                break;
-            default:
-                alert('No existe esa opción ingresada, intenta nuevamente');
-                break;
-        }
-}else{
-    alert('No existe esa opción ingresada, intenta nuevamente');
+        case '3':
+            miCarrito();
+            break; 
+        default:
+            alert('Ingresaste una opción no válida, intenta nuevamente.')
+            break;    
+    }
 }
 
+function Consulta(){
+    //Consulta de stock, agregar un carrito de compra.
+    //Objeto: Nombre, autor, editorial, costo.
+    let mensajeStock= 'Estos son los libros a disposición: \n'
+    for(let i=0; i<libroStock.length; i++ ){
+        mensajeStock+= i + '-' + libroStock[i].nombre + ' ~ ' + libroStock[i].autor + '\n';
+    };
+    let libroElegido = prompt(mensajeStock);
+    let eleccion = prompt('Tu libro elegido es: ' + libroStock[libroElegido].nombre + ' ~ ' + libroStock[libroElegido].autor + ' de la editorial ' + libroStock[libroElegido].editorial + '. El costo de tu libro es: ' + libroStock[libroElegido].costo+'.' + '\nDeseas agregarlo a tu carrito?\n1-Si \n2-No');
+    if(eleccion=='1'){
+        carritoLibro.push(libroElegido);
+        let preguntaCarrito= prompt('Deseas agregar otro libro? \n1-Si \n-2No');
+        switch(preguntaCarrito){
+            case '1':
+                Consulta();
+                break;
+
+            case '2':
+                menu();
+                break;
+        }
+    }
+}
+
+function Cotizacion(){
+    let precioLibro = parseInt(prompt('Ingresá el valor de tu libro'));
+    let formaDePago = prompt(`Ingresá tu metodo de pago  
+    1-Efectivo
+    2-Tarjeta de crédito`);
+
+    const precioConIva = suma(precioLibro,iva(precioLibro));
+    let precioTotal = 0;
+
+    if(formaDePago=='1'){
+        const descuentoAplicado = descuento(precioConIva, 0.05);
+        precioTotal = precioConIva-descuentoAplicado;
+        alert(precioTotal); 
+
+
+    } else if (formaDePago==2){
+        let debitoCredito = prompt('Va a ser con debito o credito? \n1-Débito \n2-Crédito');
+            switch (debitoCredito) {
+                case '1':
+                    //Pago con tarjeta debito
+                    let santanderTarjeta = prompt('Vas a pagar con santanderio rio? \n1-Si \n2-No');
+                    switch(santanderTarjeta){
+                        case '1':
+                            //Pago con santander rio
+                            const descuentoSantander = descuento(precioConIva, 0.10);
+                            precioTotal = precioConIva-descuentoSantander;
+                            alert(precioTotal);
+                            break;
+                        case '2':
+                            //Pago con otra tarjeta
+                            precioTotal = precioConIva;
+                            alert(precioTotal);
+                            break;
+                        default:
+                        alert('No existe esa opción ingresada, intenta nuevamente');
+                        break;
+                    }
+                    break;
+                case '2':
+                    //Pago con tarjeta de credito
+                    let cantidadCuotas = prompt('En cuantas cuotas vas a abonar? \n1-1 cuota \n2-3 cuotas \n3-6 cuotas')
+                    let precioconInteres = 0;
+                    let precioCuota =0;
+                    switch(cantidadCuotas){
+                        case '1':
+                            //1 cuota
+                            precioTotal = precioConIva;
+                            alert(precioTotal);
+                            break;
+                        case '2':
+                            //3 cuotas
+                            precioconInteres = interes(precioConIva, 0.08);
+                            precioTotal = precioConIva+precioconInteres;
+                            precioCuota= precioTotal/3
+                            alert('El valor total de tu compra es ' + precioTotal + ' y el valor de tus cuotas es: ' + precioCuota);
+                            break;
+                        case '3':
+                            //6 cuotas
+                            precioconInteres = interes(precioConIva, 0.12);
+                            precioTotal = precioConIva+precioconInteres;
+                            precioCuota= precioTotal/6
+                            alert('El valor total de tu compra es ' + precioTotal + ' y el valor de tus cuotas es: ' + precioCuota);
+                            break;
+                        default:
+                        alert('No existe esa opción ingresada, intenta nuevamente');
+                        break;
+                    }
+                    break;
+                default:
+                    alert('No existe esa opción ingresada, intenta nuevamente');
+                    break;
+            }
+    }else{
+        alert('No existe esa opción ingresada, intenta nuevamente');
+    }
+}
+
+function miCarrito(){
+    let mensajeCarrito= 'Estos son los libros en tu carrito: \n'
+    for(let i=0; i<carritoLibro.length; i++ ){
+        //carritolibro guarda la posicion del libro en libroStock.
+        mensajeCarrito+= i + '-' + libroStock[carritoLibro[i]].nombre + ' ~ ' + libroStock[carritoLibro[i]].autor + ' ' + libroStock[carritoLibro[i]].costo + '\n';
+    };
+    alert(mensajeCarrito);
+    let eliminarCarrito= prompt('Desea eliminar alguno? \n1-Si \n2-No');
+
+    switch(eliminarCarrito){
+        case '1':
+            let libroEliminado= prompt(mensajeCarrito + '\nCuál?');
+            carritoLibro.splice(libroEliminado,1);
+            miCarrito();
+            break;
+        case '2':
+            menu();
+            break;
+    }
+}
+
+
+
+
+
+
+menu();
