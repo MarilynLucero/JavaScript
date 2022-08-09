fccf2a5f3ce4f62ab66bc3e5e5bf34e281d4e697
Marilyn Lucero <lu_tina_lu@hotmail.com>
2022-08-09 01:59:50 -0300

    json y storage

--- js/script.js
+++ js/script.js
@@ -17,16 +17,16 @@ const contentcotizar = document.querySelector("#contenidoCotizar");
 const carritoDelete = document.querySelector("#carritoDelete")
 const librosVerificar = document.querySelector("#librosVerificar")
 
-const carritoLibro = [];
+let carritoLibro = [];
 const libroStock = [{
-    id: "libroA",  
+    id: "libroA",
     nombre: 'Cementerio De Animales',
     autor: 'Stephen King',
     editorial: 'Alfaguara',
     costo: '$5100',
     stock: 2
 }, {
-    id: "libroB", 
+    id: "libroB",
     nombre: 'Battle Royale',
     autor: 'Koushun Takami',
     editorial: 'Booket',
@@ -68,95 +68,169 @@ function consulta() {
     //Objeto: Nombre, autor, editorial, costo.
     let mensajeStock = `<h1>Estos son los libros a disposición. \n</h1><ul class="list-group">`
     for (let i = 0; i < libroStock.length; i++) {
-        mensajeStock +=`<li class="list-group-item list-group-item-primary"> ${libroStock[i].nombre} ~ ${libroStock[i].autor} ~ ${libroStock[i].editorial}. 
+        mensajeStock += `<li class="list-group-item list-group-item-primary"> ${libroStock[i].nombre} ~ ${libroStock[i].autor} ~ ${libroStock[i].editorial}. 
         ${libroStock[i].costo} 
         <button id=${libroStock[i].id} type="button" class="btn btn-primary agregando">Agregar</button>
          </li>`
     };
-    mensajeStock+=`</ul>`;
+    mensajeStock += `</ul>`;
     librosVerificar.innerHTML = mensajeStock;
     const agregando = document.getElementsByClassName("agregando");
-    for(let i = 0; i < agregando.length; i++){
-        agregando[i].addEventListener("click", ()=>{Agregar(agregando[i].id)});
+    for (let i = 0; i < agregando.length; i++) {
+        agregando[i].addEventListener("click", () => {
+            Agregar(agregando[i].id)
+        });
     };
-    
-}
+
+};
 
 function cotizacion() {
-    let precioLibro = parseInt(prompt('Ingresá el valor de tu libro'));
-    let formaDePago = prompt(`Ingresá tu metodo de pago  
-    1-Efectivo
-    2-Tarjeta de crédito`);
+    contentcotizar.innerHTML = `
+     <h3>Ingresá el valor de tu libro</h3>
+        <div class="input-group mb-3 campo">
+        <input id="campoCotizar1" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+     </div>
+     <button id="botonCotizar1" type="button" class="btn btn-warning">Continuá</button>`
+    document.getElementById("botonCotizar1").addEventListener('click', () => {
+        let precioLibro = parseInt(document.getElementById("campoCotizar1").value);
+        cotizacionMetodo(precioLibro);
+    });
+}
 
-    const precioConIva = suma(precioLibro, iva(precioLibro));
-    let precioTotal = 0;
+function cotizacionMetodo(precioLibro) {
+    contentcotizar.innerHTML = `
+    <h3>Ingresá tu metodo de pago.</h3>
+    <div class="form-check">
+    <input class="form-check-input" type="radio" name="metododepago" id="efectivo">
+    <label class="form-check-label" for="efectivo">
+      Efectivo
+    </label>
+    </div>
+    <div class="form-check">
+    <input class="form-check-input" type="radio" name="metododepago" id="tarjeta" checked>
+    <label class="form-check-label" for="tarjeta">
+      Tarjeta de crédito
+    </label>
+     </div>
+    <button id="botonCotizar2" type="button" class="btn btn-warning">Continuá</button>`
+    document.getElementById("botonCotizar2").addEventListener('click', () => {
+        let formaDePago = "";
+        if (true == document.getElementById("efectivo").value) {
+            formaDePago = "1";
+        } else {
+            formaDePago = "2";
+        }
+        const precioConIva = suma(precioLibro, iva(precioLibro));
+        let precioTotal = 0;
 
-    if (formaDePago == '1') {
-        const descuentoAplicado = descuento(precioConIva, 0.05);
-        precioTotal = precioConIva - descuentoAplicado;
-        contentcotizar.innerHTML = `<h1>El valor de tu libro es $${precioTotal}</h1>`;
+        if (formaDePago == '1') {
+            const descuentoAplicado = descuento(precioConIva, 0.05);
+            precioTotal = precioConIva - descuentoAplicado;
+            contentcotizar.innerHTML = `<h1>El valor de tu libro es $${precioTotal}</h1>`;
 
 
-    } else if (formaDePago == 2) {
-        let debitoCredito = prompt('Va a ser con debito o credito? \n1-Débito \n2-Crédito');
+        } else {
+            cotizacionTarjeta(precioConIva);
+        }
+    });
+
+};
+
+function cotizacionTarjeta(precioConIva) {
+    contentcotizar.innerHTML = `
+    <h3>Ingresá tu metodo de pago.</h3>
+    <div class="form-check">
+    <input class="form-check-input" type="radio" name="creditoDebito" id="debito">
+    <label class="form-check-label" for="debito">
+      Débito
+    </label>
+    </div>
+    <div class="form-check">
+    <input class="form-check-input" type="radio" name="creditoDebito" id="credito" checked>
+    <label class="form-check-label" for="credito">
+      Crédito
+    </label>
+     </div>
+    <button id="botonCotizar3" type="button" class="btn btn-warning">Continuá</button>`
+    document.getElementById("botonCotizar3").addEventListener('click', () => {
+        let debitoCredito = "";
+        if (true == document.getElementById("debito").value) {
+            debitoCredito = "1";
+        } else {
+            debitoCredito = "2";
+        }
         switch (debitoCredito) {
             case '1':
-                //Pago con tarjeta debito
-                let santanderTarjeta = prompt('Vas a pagar con santanderio rio? \n1-Si \n2-No');
-                switch (santanderTarjeta) {
-                    case '1':
-                        //Pago con santander rio
-                        const descuentoSantander = descuento(precioConIva, 0.10);
-                        precioTotal = precioConIva - descuentoSantander;
-                        contentcotizar.innerHTML = `<h1>El valor de tu libro es $${precioTotal}</h1>`;
-                        break;
-                    case '2':
-                        //Pago con otra tarjeta
-                        precioTotal = precioConIva;
-                        contentcotizar.innerHTML = `<h1>El valor de tu libro es $${precioTotal}</h1>`;
-                        break;
-                    default:
-                        alertdiv.innerHTML = `<h1>Ingresaste una opción no válida, intenta nuevamente.</h1>`;
-                        break;
-                }
+                //Pago con otra tarjeta
+                precioTotal = precioConIva;
+                contentcotizar.innerHTML = `<h1>El valor de tu libro es $${precioTotal}</h1>`;
                 break;
             case '2':
-                //Pago con tarjeta de credito
-                let cantidadCuotas = prompt('En cuantas cuotas vas a abonar? \n1-1 cuota \n2-3 cuotas \n3-6 cuotas')
-                let precioconInteres = 0;
-                let precioCuota = 0;
-                switch (cantidadCuotas) {
-                    case '1':
-                        //1 cuota
-                        precioTotal = precioConIva;
-                        contentcotizar.innerHTML = `<h1>El valor de tu libro es $${precioTotal}</h1>`;
-                        break;
-                    case '2':
-                        //3 cuotas
-                        precioconInteres = interes(precioConIva, 0.08);
-                        precioTotal = precioConIva + precioconInteres;
-                        precioCuota = precioTotal / 3
-                        contentcotizar.innerHTML = `<h1>El valor de tu libro es $${precioTotal} y el valor de tus cuotas es $${precioCuota}</h1>`;
-                        break;
-                    case '3':
-                        //6 cuotas
-                        precioconInteres = interes(precioConIva, 0.12);
-                        precioTotal = precioConIva + precioconInteres;
-                        precioCuota = precioTotal / 6
-                        contentcotizar.innerHTML = `<h1>El valor de tu libro es $${precioTotal} y el valor de tus cuotas es $${precioCuota}</h1>`;
-                        break;
-                    default:
-                        alertdiv.innerHTML = `<h1>Ingresaste una opción no válida, intenta nuevamente.</h1>`;
-                        break;
-                }
+                cotizacionCredito(precioConIva);
+                break;
+        }
+    });
+}
+
+function cotizacionCredito(precioConIva) {
+    //Pago con tarjeta de credito
+    contentcotizar.innerHTML = `
+    <h3>Ingresá tu metodo de pago.</h3>
+    <div class="form-check">
+    <input class="form-check-input" type="radio" name="cuotas" id="1">
+    <label class="form-check-label" for="1">
+      1 Cuota
+    </label>
+    </div>
+    <div class="form-check">
+    <input class="form-check-input" type="radio" name="cuotas" id="3" checked>
+    <label class="form-check-label" for="3">
+      3 Cuotas
+    </label>
+     </div>
+     <div class="form-check">
+    <input class="form-check-input" type="radio" name="cuotas" id="6" checked>
+    <label class="form-check-label" for="6">
+      6 Cuotas
+    </label>
+     </div>
+    <button id="botonCotizar4" type="button" class="btn btn-warning">Continuá</button>`
+    document.getElementById("botonCotizar4").addEventListener('click', () => {
+        let cantidadCuotas = "";
+        if (true == document.getElementById("1").value) {
+            cantidadCuotas = "1";
+        } else if (true == document.getElementById("3").value) {
+            cantidadCuotas = "2";
+        } else {
+            cantidadCuotas = "3";
+        }
+        let precioconInteres = 0;
+        let precioCuota = 0;
+        switch (cantidadCuotas) {
+            case '1':
+                //1 cuota
+                precioTotal = precioConIva;
+                contentcotizar.innerHTML = `<h1>El valor de tu libro es $${precioTotal}</h1>`;
+                break;
+            case '2':
+                //3 cuotas
+                precioconInteres = interes(precioConIva, 0.08);
+                precioTotal = precioConIva + precioconInteres;
+                precioCuota = precioTotal / 3
+                contentcotizar.innerHTML = `<h1>El valor de tu libro es $${precioTotal} y el valor de tus cuotas es $${precioCuota}</h1>`;
+                break;
+            case '3':
+                //6 cuotas
+                precioconInteres = interes(precioConIva, 0.12);
+                precioTotal = precioConIva + precioconInteres;
+                precioCuota = precioTotal / 6
+                contentcotizar.innerHTML = `<h1>El valor de tu libro es $${precioTotal} y el valor de tus cuotas es $${precioCuota}</h1>`;
                 break;
             default:
                 alertdiv.innerHTML = `<h1>Ingresaste una opción no válida, intenta nuevamente.</h1>`;
                 break;
         }
-    } else {
-        alertdiv.innerHTML = `<h1>Ingresaste una opción no válida, intenta nuevamente.</h1>`;
-    }
+    });
 }
 
 function miCarrito() {
@@ -173,10 +247,13 @@ function miCarrito() {
     };
     carritoDelete.innerHTML = mensajeCarrito;
     const eliminando = document.getElementsByClassName("eliminando");
-    for(let i = 0; i < eliminando.length; i++){
-        eliminando[i].addEventListener("click", ()=>{Eliminar(eliminando[i].id)});
+    for (let i = 0; i < eliminando.length; i++) {
+        eliminando[i].addEventListener("click", () => {
+            Eliminar(eliminando[i].id)
+        });
     };
-}
+    guardeCarrito();
+};
 
 
 //buscar libro
@@ -195,38 +272,51 @@ function buscaLibro() {
     };
     contentstock.innerHTML = mensajeBuscar;
     const agregandodeStock = document.getElementsByClassName("agregandodeStock");
-    for(let i = 0; i < agregandodeStock.length; i++){
-        agregandodeStock[i].addEventListener("click", ()=>{Agregar(agregandodeStock[i].id)});
+    for (let i = 0; i < agregandodeStock.length; i++) {
+        agregandodeStock[i].addEventListener("click", () => {
+            Agregar(agregandodeStock[i].id)
+        });
     };
-}
+    guardeCarrito();
+};
 
 //Agregar
 function buscarporId(array, id) {
     for (let i = 0; i < array.length; i++) {
-      if (array[i].id === id) {
-        return i;
-      }
-    }
-}
+        if (array[i].id === id) {
+            return i;
+        }
+    };
+};
 
-function Agregar(id){
-    console.log(id);
-    let libroAgregar = buscarporId(libroStock,id);
+function Agregar(id) {
+    let libroAgregar = buscarporId(libroStock, id);
     carritoLibro.push(libroAgregar);
-    alertdiv.innerHTML=`<div class="alert alert-warning alert-dismissible fade show" role="alert">
+    alertdiv.innerHTML = `<div class="alert alert-warning alert-dismissible fade show" role="alert">
     Libro agregado con éxito.
     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
   </div>`
-  console.log(carritoLibro);
-}
+};
 
 //Eliminar
-function Eliminar(id){
-    let libroEliminar = buscarporId(libroStock,id);
-    libroEliminar=carritoLibro.indexOf(libroEliminar);
+function Eliminar(id) {
+    let libroEliminar = buscarporId(libroStock, id);
+    libroEliminar = carritoLibro.indexOf(libroEliminar);
     carritoLibro.splice(libroEliminar, 1);
-    alertdiv.innerHTML=`<div class="alert alert-warning alert-dismissible fade show" role="alert">
+    alertdiv.innerHTML = `<div class="alert alert-warning alert-dismissible fade show" role="alert">
     Libro Eliminado con éxito.
     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
   </div>`
-}
+    guardeCarrito();
+};
+
+//Guardar carrito.
+
+function guardeCarrito() {
+    sessionStorage.setItem('carrito', JSON.stringify(carritoLibro));
+};
+
+function cargarCarrito(){
+    carritoLibro= JSON.parse(sessionStorage.getItem('carrito'));
+};
+cargarCarrito();    
